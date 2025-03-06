106b4c64bc3b4f7be704347603e53600834eea00
agnaey <agnaeyviswanath1234@gmail.com>
2025-03-06 20:13:17 +0530

    started django

---
+++ api/app/serializers.py
@@ -0,0 +1,7 @@
+# from rest_framework import serializers
+# from . models import *
+
+# class Todo_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=Api_tester
+#         fields='__all__'
